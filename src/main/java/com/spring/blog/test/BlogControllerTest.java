b9c658f61c61ac00d507ee399b4be1c157d3b017
Your Name <you@example.com>
2021-08-03 14:17:36 +0900

    환경세팅완료

---
+++ src/main/java/com/spring/blog/test/BlogControllerTest.java
@@ -0,0 +1,17 @@
+package com.spring.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//스프링이 com.spring.blog 패키지 이하를 스캔해서 모든 파일을 메모리에서 new하는 것은 아니고
+//특정 어노테이션이 붙어있는 클래스 파일들을 new 해서 IOC 스프링 컨테이에 괴리해줌
+@RestController
+public class BlogControllerTest {
+
+	//http://localhost:8088/test/hello
+	@GetMapping("test/hello")
+	public String hello() {
+		return "<h1>hello spring boot</h1>";
+	}
+}
